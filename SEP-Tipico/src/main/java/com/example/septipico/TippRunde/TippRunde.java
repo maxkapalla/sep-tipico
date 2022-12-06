8ee1c3988e0dfd269386320f6de256c41e70bc72
Lukas Lucky <lukas.lucky@stud.uni-due.de>
2022-12-06 23:07:52 +0100

    Backend TippRunde

--- SEP-Tipico/src/main/java/com/example/septipico/TippRunde/TippRunde.java
+++ SEP-Tipico/src/main/java/com/example/septipico/TippRunde/TippRunde.java
@@ -8,4 +8,29 @@ public class TippRunde {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private Long id;
+
+    @Column(name= "tipprundename")
+    private String tipprundeName;
+
+    @Column(name= "password")
+    private String password;
+
+    @Column(name= "Zugang")
+    private String zugang;
+
+    @Column(name= "Besitzer")
+    private Long besitzer;
+
+    @Column(name= "Liga")
+    private Long liga;
+
+    public TippRunde() {
+    }
+    public TippRunde(String tipprundeName, String password, String zugang, Long besitzer, Long liga) {
+        this.tipprundeName= tipprundeName;
+        this.password= password;
+        this.zugang= zugang;
+        this.besitzer= besitzer;
+        this.liga= liga;
+    }
 }
--- SEP-Tipico/src/main/java/com/example/septipico/TippRunde/TippRundeRepository.java
+++ SEP-Tipico/src/main/java/com/example/septipico/TippRunde/TippRundeRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 @Repository
 public interface TippRundeRepository extends JpaRepository<TippRunde, Long> {
 
-    
+
 }
