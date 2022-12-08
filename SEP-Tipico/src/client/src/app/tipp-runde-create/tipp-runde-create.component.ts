d3f43aa7b83f316a76fccef4c709af6fba719194
Lukas Lucky <lukas.lucky@stud.uni-due.de>
2022-12-08 15:54:16 +0100

    TippRunde create fast fertig

---
+++ SEP-Tipico/src/client/src/app/tipp-runde-create/tipp-runde-create.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute,Router} from "@angular/router";
+import {TippRunde} from "../Models/TippRunde";
+import {TippRundeService} from "../services/tipp-runde.service";
+
+@Component({
+  selector: 'app-tipp-runde-create',
+  templateUrl: './tipp-runde-create.component.html',
+  styleUrls: ['./tipp-runde-create.component.scss']
+})
+export class TippRundeCreateComponent implements OnInit {
+
+  tippRunden: TippRunde[];
+  deleteTippRunde: TippRunde;
+  createTippRunde: TippRunde;
+
+  constructor(private route: ActivatedRoute, private TippRundeService: TippRundeService, private router: Router) {
+    this.tippRunden= [];
+    this.deleteTippRunde= new TippRunde;
+    this.createTippRunde= new TippRunde;
+  }
+
+  ngOnInit(): TippRunde[] {
+    this.TippRundeService.getAll().subscribe((data:any) => this.tippRunden=data);
+    return this.tippRunden;
+  }
+
+  CreateTippRunde() {
+    this.TippRundeService.create(this.createTippRunde).subscribe(result => this.gotoTippRunde(),this.errorWithSubmit);
+  }
+
+  DeleteTippRunde() {
+    this.TippRundeService.delete(this.deleteTippRunde).subscribe(()=>this.ngOnInit());
+  }
+
+  gotoTippRunde() {
+    alert("Tipprunde erfolgreich erstellt!")
+  }
+
+  errorWithSubmit(){
+    alert("Etwas ist schief gelaufen")
+  }
+}
