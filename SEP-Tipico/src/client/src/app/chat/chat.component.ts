a36499af55fc60aba643db779e9cff358b454d06
Jan Müller <jan.mueller.due@stud.uni-due.de>
2023-01-23 23:16:50 +0100

    dropdown fix und chat veränderung

--- SEP-Tipico/src/client/src/app/chat/chat.component.ts
+++ SEP-Tipico/src/client/src/app/chat/chat.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
 
+  chatNum = 1;
+
   constructor() { }
 
   ngOnInit(): void {
+    // get all chats and check if any Long in List is my id
+    // get chats where my id is in chats[]
+    // give chatNum the value of chats.length
+  }
+
+  //param is the chat that was clicked on
+  onClick(){
+
   }
 
 }
