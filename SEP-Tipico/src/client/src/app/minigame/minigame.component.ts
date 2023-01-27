17a8bd2e6f7810c203fae9005462752d9d31367e
Max Kapalla <mkapalla@iktsolutions.de>
2023-01-27 13:51:04 +0100

    spiel optimiert

--- SEP-Tipico/src/client/src/app/minigame/minigame.component.ts
+++ SEP-Tipico/src/client/src/app/minigame/minigame.component.ts
@@ -10,20 +10,55 @@ export class MinigameComponent implements OnInit {
 
 
   kontostand: bigint;
+  visible = false;
+  root: any;
 
+  private timeout: any;
 
   constructor(private nutzerService: NutzerService) {
     this.kontostand = BigInt(sessionStorage.getItem("kontostand") + "");
+    this.root = document.querySelector(':root');
+
+
   }
 
   ngOnInit(): void {
+    this.initAnimation();
+  }
 
+  initAnimation() {
+    this.visible = false;
+    this.timeout = setTimeout(() => {
+      let size = Math.random() * (200 - 20) + 20;
+      let pos_top = Math.random() * (100);
+      let pos_left = Math.random() * (100);
+      this.root.style.setProperty('--football-size', size + 'px')
+      this.root.style.setProperty('--pos-top', pos_top + '%')
+      this.root.style.setProperty('--pos-left', pos_left + '%')
+      this.makeVisible()
+      setTimeout(() => {
+        this.makeInvisible()
+        this.initAnimation()
+      }, 5000)
+    }, 5000)
+
+  }
+
+  makeVisible() {
+    this.visible = true;
+  }
+
+  makeInvisible() {
+    this.visible = false;
   }
 
   onClick(): void {
     this.kontostand = this.kontostand + BigInt("100");
     this.nutzerService.setKontostand(sessionStorage.getItem("id") + "", String(this.kontostand)).subscribe();
     sessionStorage.setItem("kontostand", String(this.kontostand));
+    this.visible = false;
+    clearTimeout(this.timeout)
+    this.initAnimation();
   }
 
 }
