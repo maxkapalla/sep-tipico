eae1bd6a80dac302e828d6325206e98c3fc1f49e
Jan Müller <jan.mueller.due@stud.uni-due.de>
2023-01-31 13:41:15 +0100

    getGewinner fix

--- SEP-Tipico/src/main/java/com/example/septipico/TippRunde/TippRunde.java
+++ SEP-Tipico/src/main/java/com/example/septipico/TippRunde/TippRunde.java
@@ -100,7 +100,7 @@ public class TippRunde {
         this.gewDiff=gewDiff;
     }
     public String getGewGewinner() {
-        return gewTore;
+        return gewGewinner;
     }
     public void setGewGewinner(String gewGewinner) {
         this.gewGewinner=gewGewinner;
